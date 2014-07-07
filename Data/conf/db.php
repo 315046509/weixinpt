53e6d7b93296574f981ecba0b373e3faf7811d54
shichao <315046509@qq.com>
2014-07-07 13:30:42 +0800

    tijiao

--- Data/conf/db.php
+++ Data/conf/db.php
@@ -3,8 +3,8 @@ return array(
 	'DB_TYPE'=>'mysql',
 	'DB_HOST'=>'localhost',
 	'DB_PORT'=>'3306',
-	'DB_NAME'=>'aaa3669955', 
-	'DB_USER'=>'aaa3669955',
-	'DB_PWD'=>'123456a',
+	'DB_NAME'=>'admin', 
+	'DB_USER'=>'root',
+	'DB_PWD'=>'shichao',
 	'DB_PREFIX'=>'wy_',
 );
